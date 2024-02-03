321bebda0c248907bc32f5c5c23dbd1d41c1684c
Vicky Kumar <115138275+rajputvicky1@users.noreply.github.com>
2024-02-03 13:37:07 +0000

    dataTypes

---
+++ 01_basic/01_dataTypes.js
@@ -0,0 +1,30 @@
+/* /Number
+let x=12;
+let y=25;
+console.log("sum of two value",x+y) */
+
+/* /string +number
+let a="vicky";
+let b=1;
+let c=a+b;
+console.log(c); */
+
+/* /Boolean
+let a=5;
+let b="raushan"
+let c="5";
+let d=a===c;
+console.log(d) */
+
+/* /Object
+let person={firstName:"vicky",middleName:"kumar",lastName:"Rajput"};
+console.log(typeof person,person); */
+
+/* /Array list
+const cars=["Mercedes","Bmw","volvo"]
+var x=["vicky","raushan","anuj"," "]
+c=x+cars
+console.log(c); */
+
+/* x=alert("vicky") //alert is not defined in js
+console.log(x) */
